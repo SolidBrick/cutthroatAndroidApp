a6f3230f59d3ba5882bcd7d7e33e89ab6c12a95e
Terr <terrenceyang06@gmail.com>
2025-02-09 15:15:21 -0500

    Add player name input functionality

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.widget.EditText
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,34 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+    fun addPlayer1(view: View?) {
+        val editText = findViewById<EditText>(R.id.nameInput1)
+        val text = editText.text.toString().trim()
+        if (text.isNullOrBlank()) {
+            Toast.makeText(this, "Name cannot be blank", Toast.LENGTH_SHORT).show()
+        } else {
+            println(text)
+        }
+
+    }
+    fun addPlayer2(view: View?) {
+        val editText = findViewById<EditText>(R.id.nameInput2)
+        val text = editText.text.toString().trim()
+        if (text.isNullOrBlank()) {
+            Toast.makeText(this, "Name cannot be blank", Toast.LENGTH_SHORT).show()
+        } else {
+            println(text)
+        }
+    }
+    fun addPlayer3(view: View?) {
+        val editText = findViewById<EditText>(R.id.nameInput3)
+        val text = editText.text.toString().trim()
+        if (text.isNullOrBlank()) {
+            Toast.makeText(this, "Name cannot be blank", Toast.LENGTH_SHORT).show()
+        } else {
+            println(text)
         }
     }
 }
